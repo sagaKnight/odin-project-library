091dced886bd71ee15e3c9b12fc3b1983d52a90c
Rino Miguel Sagario Quijote <149220242+sagaKnight@users.noreply.github.com>
2024-11-21 15:56:07 +1100

    Removing old repo. Creating new one with a responsive ui

--- library.js
+++ library.js
@@ -1,87 +1,53 @@
 const myLibrary = [];
-const addBookBtn = document.querySelector(".addBook");
 const dialog = document.querySelector("dialog");
-const closeBtn = document.querySelector("dialog button");
-const libraryDiv = document.querySelector(".library");
+const showButton = document.querySelector("dialog + button");
+const closeButton = document.querySelector("dialog button");
 
-function Book(name, author, totalPages, status) {
-  this.name = name;
+function Book(title, author, pages, read) {
+  this.title = title;
   this.author = author;
-  this.totalPages = totalPages;
-  this.status = status;
-  this.value = [name, author, totalPages, status];
+  this.pages = pages;
+  if (read == true) {
+    this.read = "read"
+  } else {
+    this.read = "not read"
+  }
+
+  this.info = function() {
+    return `${this.title} by ${this.author}, ${this.pages}, ${this.read}`
+  }
 }
-
-let bookHarryPotter = new Book("Harry Potter and the Philosopher's Stone","J. K. Rowling","352","Read");
-let book1984 = new Book("1984", "George Orwell", "328", "Wishlisted");
-let bookKafka = new Book("Kafka on the Shore", "Haruki Murakami", "480", "TBR");
-
-addBookToLibrary(bookHarryPotter);
-addBookToLibrary(book1984);
-addBookToLibrary(bookKafka);
-
-myLibrary.forEach((book) => {
-  printOut(book);
-});
 
 function addBookToLibrary(book) {
   myLibrary.push(book);
 }
 
-function deleteItem(e) {
-  e.parentElement.remove();
-}
-
-function changeStatus(e) {
-  const breakpoint = ","
-  const split = e.previousSibling.previousSibling.textContent.split(breakpoint);
-  if (split[3] != "Read") {
-    split[3] = "Read"
-  } else {
-    split[3] = "Unread"
+function printLibrary() {
+  for (let book of myLibrary) {
+    const newBookDiv = document.createElement("div");
+    const newTitle = document.createTextNode(book.title);
+    newBookDiv.appendChild(newTitle);
+    const libraryDiv = document.getElementById("library-content");
+    console.log(libraryDiv);
+    libraryDiv.appendChild(newBookDiv);
   }
-  e.previousSibling.previousSibling.textContent = split.toString();
 }
 
-function printOut(book) {
-  let paragraphElemBook = document.createElement("p");
-  let bookBorder = document.createElement("div");
-  bookBorder.classList.add("bookBorder");
-
-  let deleteBtn = document.createElement("button");
-  deleteBtn.textContent = "delete";
-  deleteBtn.classList.add("deleteBtn");
-
-  let editBtn = document.createElement("button");
-  editBtn.textContent = "read/unread";
-  editBtn.classList.add("editBtn");
-
-  bookBorder.appendChild(paragraphElemBook);
-  bookBorder.appendChild(deleteBtn);
-  bookBorder.appendChild(editBtn);
-  paragraphElemBook.textContent = book.value.toString();
-  libraryDiv.appendChild(bookBorder);
-
-  deleteBtn.addEventListener("click", function () {
-    deleteItem(this);
-  });
-
-  editBtn.addEventListener("click", function () {
-    changeStatus(this);
-  })
-}
-
-addBookBtn.addEventListener("click", () => {
+showButton.addEventListener("click", () => {
   dialog.showModal();
-});
+})
 
-closeBtn.addEventListener("click", () => {
-  let author = document.getElementById("author").value;
-  let title = document.getElementById("title").value;
-  let pages = document.getElementById("pages").value;
-  let status = document.getElementById("status").value;
-  let newBook = new Book(title, author, pages, status);
-  addBookToLibrary(newBook);
-  printOut(newBook);
+closeButton.addEventListener("click", () => {
   dialog.close();
-});
+})
+
+const mockingbird = new Book("To Kill a Mockingbird", "Harper Lee", 281, false);
+const harryPotter = new Book("Harry Potter and the Sorcerer's Stone", "J.K. Rowling", 309, false);
+const gatsby = new Book("The Great Gatsby", "F. Scott Fitzgerald", 180, false);
+
+addBookToLibrary(mockingbird);
+addBookToLibrary(harryPotter);
+addBookToLibrary(gatsby);
+
+printLibrary();
+
